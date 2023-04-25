4c5be312f84894d05f8416b76733b58217da043a
vadim l <vadimlozinschii@gmail.com>
2023-04-25 12:13:40 -0700

    updates majority of css and file structure

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,23 @@
 import React from "react";
 
-function Search () {
+function Search() {
     return (
         <div>
-        <h2 class="text-color border-l-amber-600"> </h2>
-        <input type="text" placeholder="Search..." />
-        <button type="button" class="shadow-inner">Search</button>
+        <div className="flex">
+            <input
+            type="text"
+            placeholder="Search..."
+            className="flex-grow px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-amber-600 mr-2"
+            />
+            <button
+            type="button"
+            className="bg-blue-600 text-white py-2 px-4 rounded-md cursor-pointer hover:bg-blue-700 ml-2"
+            >
+            Search
+            </button>
+        </div>
         </div>
     );
-};
+}
 
 export default Search;
